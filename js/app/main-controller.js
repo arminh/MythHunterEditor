7766775ad7bbbb6d4a335a5e828e5333b18b76e8
Armin Hutzler <armin.hutzler@student.tugraz.at>
2015-12-08 13:40:29 +0100

    Cleanup

--- js/app/main-controller.js
+++ js/app/main-controller.js
@@ -4,30 +4,8 @@
 
 app.controller("MainController",['$scope','$state', '$soap', function ($scope, $state, $soap) {
 
-	
-	 // Get a sessionid
-     	var backend = new backend_com_wsdl_IBackend();
+	var backend = new backend_com_wsdl_IBackend();
 	backend.url = "http://localhost:8080/Backend/webservices/Backend";
 	//backend.fillDB(function() { console.log("success"); }, function() { console.log("fail"); });
 	backend.getUser(function() { console.log("success"); }, function() { console.log("fail"); }, "1");
-
-
-	//backend_com_wsdl_fillDB_op(function() { console.log("success"); }, function() { console.log("fail"); });
-
-       //var base_url = "http://localhost:8080/Backend/webservices/Backend";
-
-
-	/*$soap.post(base_url,"fillDB").then(function(response){
-		$scope.response = response;
-		console.log(response);
-	});*/
-
-	/*$soap.post(base_url,"getUser", {arg0: "1"}).then(function(response){
-		$scope.response = response;
-		console.log(response);
-	});
-
-	IBackend.fillDB(function(response) { console.log("success"); }, function(response) { console.log("success"); });*/
-
-    
 }]);
