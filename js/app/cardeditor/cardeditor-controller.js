ecc95b0e3b3368d4027b0c4a7ee541ee981109ad
Armin Hutzler <arminhutzler@gmx.at>
2017-03-29 16:00:51 +0200

    Cardeditor validations

--- js/app/cardeditor/cardeditor-controller.js
+++ js/app/cardeditor/cardeditor-controller.js
@@ -19,6 +19,7 @@
         vm.image = {};
 
         vm.calculateStarCount = calculateStarCount;
+        vm.statMaxLife = statMaxLife;
         vm.isActionAffordable = isActionAffordable;
         vm.cardImageChanged = cardImageChanged;
         vm.confirm = confirm;
@@ -41,6 +42,13 @@
 
         function calculateStarCount() {
             vm.card.stars = CardEditorService.calculateStarCount(vm.card.attack, vm.card.life, vm.card.actions)
+        }
+
+        function statMaxLife() {
+            var starCount = CardEditorService.calculateStarCount(vm.card.attack, 0, vm.card.actions);
+            var max = Math.min(10, (10-starCount)*2);
+            console.log(max);
+            return max;
         }
 
         function cardImageChanged(e, newImage) {
